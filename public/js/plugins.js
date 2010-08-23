0a33cee084962da8dfd77aa7a7af2dfeefdfac45
Nathan L Smith <nlloyds@gmail.com>
2010-08-22 23:39:47 -0500

    es5

--- public/js/plugins.js
+++ public/js/plugins.js
@@ -1,4 +1,6 @@
 // Module to load jQuery plugins
-require.def("plugins", ["plugins/ui"]);
+require.def("plugins", ["global-es5", "plugins/ui"], function () {
+    if (typeof JSON === "undefined") { require(["json2"]); }
+});
 // TODO
-// tags, forms, global-es5, json, couch, promises, grid, hash
+// tags, forms, couch, promises, grid, hash
